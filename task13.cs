7056161104398f94418dc605336cb1754ee7c3b7
Dmitry Gulakov <essencegodof@gmail.com>
2023-02-20 18:20:13 +0300

    закончил 13 задание

---
+++ task13.cs
@@ -0,0 +1,14 @@
+/*
+опять же, вариант для натрульных чисел =)
+*/
+Console.WriteLine("Write a number:");
+string message = Console.ReadLine();
+int number = int.Parse(message);
+string answer;
+if (nimber > 99) {
+    answer = Convert.ToString(message[2]);
+}
+else {
+    answer = "There is no third digit =("
+}
+Console.WriteLine(answer);
