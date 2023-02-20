5f93307e3864d8869b7799d22ab20f53b95c4008
Dmitry Gulakov <essencegodof@gmail.com>
2023-02-20 18:17:17 +0300

    добавлен комментарий

--- task10.cs
+++ task10.cs
@@ -1,9 +1,12 @@
-
+/*
+В задании не было ничего сказано про отрицательные числа,
+поэтому я решил оставить вариант только для натуральных чисел =)
+*/
 Console.WriteLine("Write a number:");
 string message = Console.ReadLine();
 int number = int.Parse(message);
 string answer;
-if ((number >= 100) && (number <- 999)) {
+if ((number >= 100) && (number <= 999)) {
     answer = Convert.ToString(message[1]);
 }
 else {
